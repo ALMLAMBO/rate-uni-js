59ce5a2e37bad31e5cf6f1d31e9e908a86fc9002
ALMLAMBO <aivanovm@uni-sofia.bg>
2024-06-10 11:01:44 +0300

    Install @angular/fire, @angular/material, tailwindcss and uuid packages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 @NgModule({
   declarations: [
@@ -13,7 +17,18 @@ import { AppComponent } from './app.component';
     AppRoutingModule
   ],
   providers: [
-    provideClientHydration()
+    provideClientHydration(),
+    provideFirebaseApp(() => initializeApp({
+      "projectId":"rate-uni-js-project",
+      "appId":"1:947898370714:web:bd2ce7351d5d12fb94ad23",
+      "storageBucket":"rate-uni-js-project.appspot.com",
+      "apiKey":"AIzaSyApfYN7FqfGhhtB6PwBI_2Ztlb7v4ntLe8",
+      "authDomain":"rate-uni-js-project.firebaseapp.com",
+      "messagingSenderId":"947898370714"
+    })),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideAnimationsAsync()
   ],
   bootstrap: [AppComponent]
 })
