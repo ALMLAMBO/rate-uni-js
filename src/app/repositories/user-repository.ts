0c8b3721c4d0eed9180c0193197f5f578e356e7f
ALMLAMBO <aivanovm@uni-sofia.bg>
2024-06-17 14:31:27 +0300

    Add method for getting user by email

--- src/app/repositories/user-repository.ts
+++ src/app/repositories/user-repository.ts
@@ -2,6 +2,8 @@ import {BaseRepository} from "./base/base-repository";
 import {User} from "../models/base/user";
 import {environment} from "../../environments/environment.development";
 import {Injectable} from "@angular/core";
+import {Observable} from "rxjs";
+import {UserRequest} from "../models/base/user-request";
 
 @Injectable({
   providedIn: 'root'
@@ -9,5 +11,11 @@ import {Injectable} from "@angular/core";
 export class UserRepository extends BaseRepository<User> {
   constructor() {
     super(environment.userCollectionName);
+  }
+
+  getUserByEmail(email: string): Observable<User[]> {
+    return this.angularFirestore
+      .collection<User>(this.collectionName, ref => ref.where('email', '==', email))
+      .valueChanges();
   }
 }
