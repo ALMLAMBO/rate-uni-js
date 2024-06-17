1e739db707181f4b2b9cf897494660326dce12b0
ALMLAMBO <aivanovm@uni-sofia.bg>
2024-06-17 14:30:52 +0300

    Add methods for getting active requests and updating request status

--- src/app/repositories/review-request-repository.ts
+++ src/app/repositories/review-request-repository.ts
@@ -2,6 +2,8 @@ import {ReviewRequest} from "../models/base/review-request";
 import {BaseRepository} from "./base/base-repository";
 import {environment} from "../../environments/environment.development";
 import {Injectable} from "@angular/core";
+import {Observable} from "rxjs";
+import {RequestStatus} from "../vo/request-status";
 
 @Injectable({
   providedIn: 'root'
@@ -9,5 +11,19 @@ import {Injectable} from "@angular/core";
 export class ReviewRequestRepository extends BaseRepository<ReviewRequest> {
   constructor() {
     super(environment.reviewRequestCollectionName);
+  }
+
+  getActiveRequests(): Observable<ReviewRequest[]> {
+    return this.angularFirestore
+      .collection<ReviewRequest>(environment.reviewRequestCollectionName, ref => ref.where('requestStatus', '==', 'PENDING'))
+      .valueChanges();
+  }
+
+  updateRequestStatus(requestId: string, status: RequestStatus): void {
+    this.angularFirestore
+      .collection<ReviewRequest>(environment.reviewRequestCollectionName)
+      .doc(requestId)
+      .update({requestStatus: status})
+      .then(() => console.log('Request status updated'));
   }
 }
--- src/app/repositories/user-request-repository.ts
+++ src/app/repositories/user-request-repository.ts
@@ -2,6 +2,9 @@ import {BaseRepository} from "./base/base-repository";
 import {UserRequest} from "../models/base/user-request";
 import {environment} from "../../environments/environment.development";
 import {Injectable} from "@angular/core";
+import {Observable} from "rxjs";
+import {RequestStatus} from "../vo/request-status";
+import {ReviewRequest} from "../models/base/review-request";
 
 @Injectable({
   providedIn: 'root'
@@ -9,5 +12,19 @@ import {Injectable} from "@angular/core";
 export class UserRequestRepository extends BaseRepository<UserRequest> {
   constructor() {
     super(environment.userRequestCollectionName);
+  }
+
+  getActiveRequests(): Observable<UserRequest[]> {
+    return this.angularFirestore
+      .collection<UserRequest>(environment.userRequestCollectionName, ref => ref.where('requestStatus', '==', 'PENDING'))
+      .valueChanges();
+  }
+
+  updateRequestStatus(requestId: string, status: RequestStatus): void {
+    this.angularFirestore
+      .collection<ReviewRequest>(environment.reviewRequestCollectionName)
+      .doc(requestId)
+      .update({requestStatus: status})
+      .then(() => console.log('Request status updated'));
   }
 }
