49f476796c2a2fb26f8fb59d7a86a0fdc77cdc23
ALMLAMBO <aivanovm@uni-sofia.bg>
2024-06-18 13:36:45 +0300

    Create components for displaying universities, details about university and creating or updating university

---
+++ src/app/components/university/create-update-university/create-update-university.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-update-university',
+  templateUrl: './create-update-university.component.html',
+  styleUrl: './create-update-university.component.css'
+})
+export class CreateUpdateUniversityComponent {
+
+}
