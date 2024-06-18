88cfdc9139f66239571d1112adc6f13d12778ad4
ALMLAMBO <aivanovm@uni-sofia.bg>
2024-06-18 13:31:42 +0300

    Create components for displaying reviews for current discipline, details about review and creating review

---
+++ src/app/components/discipline/create-update-discipline/create-update-discipline.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-update-discipline',
+  templateUrl: './create-update-discipline.component.html',
+  styleUrl: './create-update-discipline.component.css'
+})
+export class CreateUpdateDisciplineComponent {
+
+}
