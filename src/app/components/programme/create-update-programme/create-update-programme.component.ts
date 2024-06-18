0b852f8f702c8dd79bb8b60601b024f255792fd0
ALMLAMBO <aivanovm@uni-sofia.bg>
2024-06-18 13:34:44 +0300

    Create components for displaying programmes for current faculty, details about programme and creating or updating programme

---
+++ src/app/components/programme/create-update-programme/create-update-programme.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-update-programme',
+  templateUrl: './create-update-programme.component.html',
+  styleUrl: './create-update-programme.component.css'
+})
+export class CreateUpdateProgrammeComponent {
+
+}
---
+++ src/app/components/programme/programme-details/programme-details.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-programme-details',
+  templateUrl: './programme-details.component.html',
+  styleUrl: './programme-details.component.css'
+})
+export class ProgrammeDetailsComponent {
+
+}
