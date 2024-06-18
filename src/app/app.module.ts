f94f556e56cdf7a25eee60901c7b8543bb4db09c
ALMLAMBO <aivanovm@uni-sofia.bg>
2024-06-18 13:27:27 +0300

    Create components and add them to module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,50 @@ import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { NavbarComponent } from "./components/main/navbar/navbar.component";
+import { HomeComponent } from './components/main/home/home.component';
+import { LoginComponent } from './components/user/login/login.component';
+import { RegisterComponent } from './components/user/register/register.component';
+import { UserDetailsComponent } from './components/user/user-details/user-details.component';
+import { AdminStatisticsComponent } from './components/user/admin-statistics/admin-statistics.component';
+import { UniversitiesComponent } from './components/university/universities/universities.component';
+import { UniversityDetailsComponent } from './components/university/university-details/university-details.component';
+import { CreateUpdateUniversityComponent } from './components/university/create-update-university/create-update-university.component';
+import { FacultiesComponent } from './components/faculty/faculties/faculties.component';
+import { FacultyDetailsComponent } from './components/faculty/faculty-details/faculty-details.component';
+import { ProgrammesComponent } from './components/programme/programmes/programmes.component';
+import { ProgrammeDetailsComponent } from './components/programme/programme-details/programme-details.component';
+import { CreateUpdateProgrammeComponent } from './components/programme/create-update-programme/create-update-programme.component';
+import { DisciplinesComponent } from './components/discipline/disciplines/disciplines.component';
+import { DisciplineDetailsComponent } from './components/discipline/discipline-details/discipline-details.component';
+import { CreateUpdateDisciplineComponent } from './components/faculty/create-update-discipline/create-update-discipline.component';
+import { ReviewsComponent } from './components/review/reviews/reviews.component';
+import { ReviewDetailsComponent } from './components/review/review-details/review-details.component';
+import { CreateReviewComponent } from './components/review/create-review/create-review.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    UserDetailsComponent,
+    AdminStatisticsComponent,
+    UniversitiesComponent,
+    UniversityDetailsComponent,
+    CreateUpdateUniversityComponent,
+    FacultiesComponent,
+    FacultyDetailsComponent,
+    CreateUpdateDisciplineComponent,
+    ProgrammesComponent,
+    ProgrammeDetailsComponent,
+    CreateUpdateProgrammeComponent,
+    DisciplinesComponent,
+    DisciplineDetailsComponent,
+    ReviewsComponent,
+    ReviewDetailsComponent,
+    CreateReviewComponent
   ],
   imports: [
     BrowserModule,
