8a21b58b10901573565da9d91ea102e48b9af458
ALMLAMBO <aivanovm@uni-sofia.bg>
2024-06-24 20:52:17 +0300

    Fix width of review card and create home component

--- src/app/components/main/home/home.component.ts
+++ src/app/components/main/home/home.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ReviewService} from "../../../services/business-logic/review.service";
+import {Review} from "../../../models/base/review";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  reviews: Review[] = [];
 
+  constructor(private reviewService: ReviewService) {
+
+  }
+
+  ngOnInit(): void {
+    this.reviewService.getLatest6Reviews()
+      .subscribe(reviews => {
+        this.reviews = reviews;
+      })
+  }
 }
