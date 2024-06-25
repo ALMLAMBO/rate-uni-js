933d780f0eda42edf21d7ced3157a52af8704dd3
ALMLAMBO <aivanovm@uni-sofia.bg>
2024-06-25 10:04:14 +0300

    Finish university-details component and fix router link

--- src/app/components/university/university-details/university-details.component.ts
+++ src/app/components/university/university-details/university-details.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {UniversityService} from "../../../services/business-logic/university.service";
+import {University} from "../../../models/base/university";
+import {Faculty} from "../../../models/base/faculty";
+import {FacultyService} from "../../../services/business-logic/faculty.service";
+import {environment} from "../../../../environments/environment.development";
 
 @Component({
   selector: 'app-university-details',
   templateUrl: './university-details.component.html',
   styleUrl: './university-details.component.css'
 })
-export class UniversityDetailsComponent {
+export class UniversityDetailsComponent implements OnInit {
+  university: University = {} as University;
+  faculties: Faculty[] = [];
 
+  constructor(private route: ActivatedRoute,
+              private universityService: UniversityService,
+              private facultyService: FacultyService) {
+
+  }
+
+  ngOnInit(): void {
+    let universityId = '';
+    this.route.params.subscribe(params => {
+      universityId = params[environment.urlIds.university];
+    });
+
+    this.universityService.getObjectById(universityId)
+      .subscribe((data: University) => {
+        this.university = data;
+      });
+
+    this.facultyService.getFacultyByUniversityId(universityId)
+      .subscribe((data: Faculty[]) => {
+        this.faculties = data;
+      });
+
+    console.log(this.university)
+    console.log(this.faculties)
+  }
 }
