26ac06a8772cab02abb939a2f323668081eae93e
ALMLAMBO <aivanovm@uni-sofia.bg>
2024-06-25 16:14:33 +0300

    Create faculties component and add link to universities

--- src/app/components/faculty/faculties/faculties.component.ts
+++ src/app/components/faculty/faculties/faculties.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import { Faculty } from '../../../models/base/faculty';
+import {ActivatedRoute} from "@angular/router";
+import {environment} from "../../../../environments/environment.development";
 
 @Component({
   selector: 'app-faculties',
   templateUrl: './faculties.component.html',
   styleUrl: './faculties.component.css'
 })
-export class FacultiesComponent {
+export class FacultiesComponent implements OnInit {
+  @Input() faculties: Faculty[] = [];
+  universityId: string = '';
 
+  constructor(private route: ActivatedRoute){
+    
+  }
+
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.universityId = params[environment.urlIds.university];
+    });      
+  }
 }
